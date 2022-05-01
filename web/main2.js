036b4490636e4385be4c2b8cbb7a71fefa3e9a1c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-01 16:06:36 +0100

    Sixth Commit: Implement main2.js

--- web/main2.js
+++ web/main2.js
@@ -3,4 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
 
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
+
 
