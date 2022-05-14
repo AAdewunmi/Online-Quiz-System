a83bb6a4edaf26320871522edfb8de0924e925f2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-14 20:00:07 +0100

    Fourteenth Commit: Refactored landing page.

---
+++ web/typed.js
@@ -0,0 +1,13 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+var typed = new Typed("#typed", {
+  strings: ["Welcome to Freelanco"],
+  typeSpeed: 110,
+  startDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+  backDelay: 1000
+});
