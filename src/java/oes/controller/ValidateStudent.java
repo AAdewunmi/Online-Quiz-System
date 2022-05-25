782f6086c2965f339c7eddd5d993570f86f02ea4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-25 13:11:57 +0100

    Thirty-third Commit: Implement ValidateStudent Class

--- src/java/oes/controller/ValidateStudent.java
+++ src/java/oes/controller/ValidateStudent.java
@@ -4,9 +4,11 @@
  */
 package oes.controller;
 
+import java.io.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
 
 
@@ -14,49 +16,23 @@ import jakarta.servlet.http.*;
  *
  * @author adrianadewunmi
  */
+
+//importing all packages
+import oes.db.Admins;
+import oes.db.Students;
+import oes.model.AdminsDao;
+import oes.model.StudentsDao;
+
+@WebServlet("/oes.controller.ValidateStudent")
+
 public class ValidateStudent extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ValidateStudent</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ValidateStudent at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public ValidateStudent() {
+        super();
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -68,17 +44,32 @@ public class ValidateStudent extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        
+        String username = request.getParameter("uname");
+        String password = request.getParameter("pass");
+        
+        Students sd = new Students();
+        sd.setUsername(username);
+        sd.setPassword(password);
+        
+       boolean status = StudentsDao.doValidate(sd);
+       
+       if(status)
+	    {
+	    	//Logged in  as student do something (pending)
+	    	HttpSession studentsession = request.getSession();
+                studentsession.setAttribute("username", sd.getUsername());
+                studentsession.setAttribute("name",sd.getName() );
+                response.sendRedirect("StudentInstructions.jsp");
+	    }
+	    else
+	    {
+	    	String msg = "Invalid Username or Password";
+	    	response.sendRedirect("StudentLogin.jsp?msg="+msg);
+            }
+       
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
