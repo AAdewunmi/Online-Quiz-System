66210e6f4292f10e09dfb17740ba126dfc212652
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-26 17:09:32 +0100

    Forty-Third Commit: Implement Getter And Setter For Instruction Variable In Instruction Class.

--- src/java/oes/db/Instructions.java
+++ src/java/oes/db/Instructions.java
@@ -10,4 +10,14 @@ package oes.db;
  */
 public class Instructions {
     
+    private String instruction;
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+    
 }
