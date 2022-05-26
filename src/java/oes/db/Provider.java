9f4b541b08e34415919715535e487bb7783b52a8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-26 15:47:29 +0100

    Thirty-sixth Commit: Implement Provider Class For Connecting To Online Quiz Database

--- src/java/oes/db/Provider.java
+++ src/java/oes/db/Provider.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package oes.db;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author adrianadewunmi
  */
 public class Provider {
+    
+    public static Connection getOracleConnection(){
+        Connection con;
+        con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try {
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineQuizDB", "root", "abc");
+                System.out.println("Connection Successful, Yippe!!!");
+            } catch (SQLException ex) {
+                System.err.println("ERROR -> SQLException");
+                Logger.getLogger(Provider.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            System.err.println("ERROR -> ClassNotFoundException");
+            Logger.getLogger(Provider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
     
 }
