e9a47ff49fd464a6b7908f222cd8b65ef893cc25
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-26 16:59:08 +0100

    Forty-First Commit: Implement Getters and Setters For Question And Answer In Questions Class.

--- src/java/oes/db/Questions.java
+++ src/java/oes/db/Questions.java
@@ -10,4 +10,60 @@ package oes.db;
  */
 public class Questions {
     
+    private String question;
+    private String answer;
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+   
+    
 }
