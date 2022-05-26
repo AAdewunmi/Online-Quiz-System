bc3d91a4ef82a0884aeaada118bb78d2edbd428e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-26 16:28:39 +0100

    Thirty-eighth Commit: Implement Getting And Setting Administrator Username And Password

--- src/java/oes/db/Admins.java
+++ src/java/oes/db/Admins.java
@@ -10,4 +10,25 @@ package oes.db;
  */
 public class Admins {
     
+    public static int precount = 0;
+    
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
