6652f70b504de6944e023683d3eec0372e318039
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-27 11:30:31 +0100

    Forty-Fifth Commit: Implement doValidate() in StudentsDao Class.

--- src/java/oes/model/StudentsDao.java
+++ src/java/oes/model/StudentsDao.java
@@ -4,10 +4,43 @@
  */
 package oes.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import oes.db.Provider;
+import oes.db.Students;
+
 /**
  *
  * @author adrianadewunmi
  */
 public class StudentsDao {
+    
+    public static boolean doValidate(Students sd){
+        boolean status = false;
+        try{
+            
+            Connection con = Provider.getOracleConnection();
+            String sql = "select * from studenttable where userid=? and password=?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, sd.getUsername());
+            pst.setString(2, sd.getPassword());
+            ResultSet rs = pst.executeQuery();
+            
+                if(rs.next()){
+                    sd.setName(rs.getString("name"));
+                    status = true;
+                }else{
+                    status = false;
+                    System.out.println("ERROR! -> Connection status = false");
+                }
+            
+        }catch(SQLException e){
+            System.out.println("ERROR! -> SQLException");
+            System.out.println(e);
+        }
+        return status;
+    }
     
 }
