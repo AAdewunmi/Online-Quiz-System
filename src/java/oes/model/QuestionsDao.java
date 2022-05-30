1e85c799af7199d78ee384baf061f3c8eb553981
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-05-30 20:13:53 +0100

    Fifty-Second: Implement insertQuestion() in QuestionDao Class, for inserting values into questiontable.

--- src/java/oes/model/QuestionsDao.java
+++ src/java/oes/model/QuestionsDao.java
@@ -4,10 +4,43 @@
  */
 package oes.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import oes.db.Provider;
+import oes.db.Questions;
+
 /**
  *
  * @author adrianadewunmi
  */
 public class QuestionsDao {
+    
+    public static boolean insertQuestion(Questions q){
+        boolean status = false;
+        try{
+            Connection con = Provider.getOracleConnection();
+            String sql = "insert into questiontable values(?,?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, q.getQuestion().trim());
+            pst.setString(2, q.getA());
+            pst.setString(3, q.getB());
+            pst.setString(4, q.getC());
+            pst.setString(5, q.getD());
+            pst.setString(6, q.getAnswer());
+            
+            int val = pst.executeUpdate();
+            if(val > 0){
+                status = true;
+            }else{
+                status = false;
+                System.out.println("ERROR! -> Connection status = false");
+            }
+        }catch(SQLException e){
+            System.out.println("ERROR! -> SQLException");
+            System.out.println(e);
+        }
+        return status;
+    }
     
 }
