30aaff3fea2db289aa56f2e59170eb22b8b78dea
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-06-01 16:43:32 +0100

    Fifty-Seventh: Implement insertInstruction(), which inserts data into instructiontable table.

--- src/java/oes/model/InstructionsDao.java
+++ src/java/oes/model/InstructionsDao.java
@@ -4,10 +4,40 @@
  */
 package oes.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import oes.db.Instructions;
+import oes.db.Provider;
+
 /**
  *
  * @author adrianadewunmi
  */
 public class InstructionsDao {
+    
+    public static boolean insertInstruction(Instructions ist){
+        
+        boolean status = false;
+        
+        try{
+            Connection con = Provider.getOracleConnection();
+            String sql = "insert into instructiontable values(?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, ist.getInstruction());
+            
+            int val = pst.executeUpdate();
+            if(val > 0){
+                status = true;
+            }else{
+                status = false;
+                System.out.println("ERROR! -> Connection status = false");
+            }
+        }catch(SQLException e){
+            System.out.println("ERROR! -> SQLException");
+            System.out.println(e);
+        }
+        return status;
+    }
     
 }
