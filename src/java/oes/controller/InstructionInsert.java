aa71d1b2fc910f04dc0b8a8d49dbf50aabeba046
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-06-05 10:50:11 +0100

    Sixty-Second: Implement InstructionInsert Class for inserting instructions into database.

--- src/java/oes/controller/InstructionInsert.java
+++ src/java/oes/controller/InstructionInsert.java
@@ -5,45 +5,30 @@
 package oes.controller;
 
 import java.io.IOException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import oes.db.Instructions;
+import oes.model.InstructionsDao;
 
 /**
  *
  * @author adrianadewunmi
  */
+
+@WebServlet("/oes.controller.InstructionInsert")
+
 public class InstructionInsert extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InstructionInsert</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InstructionInsert at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public InstructionInsert() {
+        super();
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -55,31 +40,25 @@ public class InstructionInsert extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        
+        Instructions ist = new Instructions();
+        ist.getInstruction();
+        
+        String instruction = request.getParameter("inst");
+        
+        boolean status = InstructionsDao.insertInstruction(ist);
+        
+        if(status){
+            String msg1 = "Instruction Added!";
+            response.sendRedirect("AddInstruction.jsp?msg1="+msg1);
+        }else{
+              String msg2 = "Instruction Not Added!";
+              response.sendRedirect("AddInstruction.jsp?msg2="+msg2);  
+        }
+           
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
