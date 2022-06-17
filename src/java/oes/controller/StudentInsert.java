ec343f0c9456d3a0d701111fa9b92888cec7422a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-06-17 13:43:07 +0100

    Seventy-First Commit: Implement StudentInsert Class, which contains method for adding a new student to database.

--- src/java/oes/controller/StudentInsert.java
+++ src/java/oes/controller/StudentInsert.java
@@ -7,40 +7,22 @@ package oes.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import oes.db.Students;
+import oes.model.StudentsDao;
 
 /**
  *
  * @author adrianadewunmi
  */
+@WebServlet("/oes.controller.StudentInsert")
 public class StudentInsert extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet StudentInsert</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet StudentInsert at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public StudentInsert() {
+        super();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -55,31 +37,28 @@ public class StudentInsert extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        
+        String username = request.getParameter("uname");
+        String password = request.getParameter("pass");
+        String name = request.getParameter("name");
+        
+        Students st = new Students();
+        st.setName(name);
+        st.setPassword(password);
+        st.setUsername(username);
+        
+        boolean status = StudentsDao.insertStudent(st);
+        
+        if(status){
+            String msg1 = "Student Added!";
+            response.sendRedirect("AddStudent.jsp?msg1="+msg1);
+        }else{
+            String msg2 = "Student Not Added!";
+            response.sendRedirect("AddStudent.jsp?msg2="+msg2);
+        }
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
