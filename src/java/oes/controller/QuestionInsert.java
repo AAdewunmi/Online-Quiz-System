9f9f84ec9136bd573a5afa6307ba5979b61615ba
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-06-17 12:44:49 +0100

    Sixty-ninth Commit: Implement QuestionInsert Class, for inserting new questions.

--- src/java/oes/controller/QuestionInsert.java
+++ src/java/oes/controller/QuestionInsert.java
@@ -7,42 +7,25 @@ package oes.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import oes.db.Questions;
+import oes.model.QuestionsDao;
 
 /**
  *
  * @author adrianadewunmi
  */
+@WebServlet("/oes.controller.QuestionInsert")
 public class QuestionInsert extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet QuestionInsert</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet QuestionInsert at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public QuestionInsert() {
+        super();
     }
-
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -55,31 +38,32 @@ public class QuestionInsert extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        String question = request.getParameter("Question");
+        String a = request.getParameter("option1");
+        String b = request.getParameter("option2");
+        String c = request.getParameter("option3");
+        String d = request.getParameter("option4");
+        String answer = request.getParameter("answer");
+        
+        Questions q = new Questions();
+        q.setA(a);
+        q.setB(b);
+        q.setC(c);
+        q.setD(d);
+        q.setAnswer(answer);
+        q.setQuestion(question);
+        
+        boolean status = QuestionsDao.insertQuestion(q);
+        if(status){
+            String msg1 = "Question Added!";
+            response.sendRedirect("AddQuestion.jsp?msg1="+msg1);
+        }else{
+            String msg2 = "Question Not Added!";
+            response.sendRedirect("AddQuestion.jsp?msg2="+msg2);
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
