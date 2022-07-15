658828e3d02c7fe862a9090ee0f0224502b67074
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-07-15 12:26:38 +0100

    One Hundred-Twenty Five Commit: Fixed 404 Error in LogoutAdmin Class by migrating from Java EE to Jakarta EE.

--- src/java/oes/controller/LogoutAdmin.java
+++ src/java/oes/controller/LogoutAdmin.java
@@ -4,13 +4,16 @@
  */
 package oes.controller;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
 
 /**
  *
