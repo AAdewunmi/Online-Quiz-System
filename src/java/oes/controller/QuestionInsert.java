8549564bc262ef7d2f13226814cfd31e9d12214e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-07-15 12:36:25 +0100

    One Hundred-Twenty Six Commit: Fixed 404 Error in QuestionInsert Class by migrating from Java EE to Jakarta EE.

--- src/java/oes/controller/QuestionInsert.java
+++ src/java/oes/controller/QuestionInsert.java
@@ -6,11 +6,17 @@ package oes.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
 import oes.db.Questions;
 import oes.model.QuestionsDao;
 
